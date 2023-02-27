2e03937ab29c985850453761adb5cfaa12882f5e
ANullchance <92113036+2xMoeKyun@users.noreply.github.com>
2023-02-27 20:49:42 +0300

    need to fix tabs creating

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 import asyncio
+import itertools
 import os
 import shutil
 import json
+import sys
 import threading
 import multiprocessing
 import time
 from random import randint
 
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 
+from kivymd.uix.label import MDLabel
 from kivymd.uix.filemanager import MDFileManager
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.list import TwoLineListItem
-from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.tab import MDTabsBase, MDTabs
+
+# from android.storage import app_storage_path
+# settings_path = app_storage_path()
+
+
 
 Window.size = (300, 608)
 
 
 class Tab(MDFloatLayout, MDTabsBase):
     """Class implementing content for a tab."""
+    main_widget = None
+
     def __init__(self, **kwargs):
         super(Tab, self).__init__(**kwargs)
+        al = AnchorLayout(anchor_y='top', anchor_x='left',padding=15)
+        self.main_widget = GridLayout(cols=1, size_hint_y=0.4)
+        al.add_widget(self.main_widget)
+        self.add_widget(al)
+
+    def add_label(self, text):
+        label = MDLabel(font_style='H5', size_hint=(1,0.1), text=text)
+        self.main_widget.add_widget(label)
 
 
 class MainWindow(Screen):
@@ -51,7 +71,6 @@ class WindowManager(ScreenManager):
         'position': 'none',
         'phone': 'none',
     }
-
     index = 0
 
     def __init__(self, **kwargs):
@@ -64,20 +83,44 @@ class WindowManager(ScreenManager):
             preview=True,
         )
         self.index = self.__get_index()
-        # self.update_list()  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    def __exit(self):
+        sys.exit()
 
     def create_list_widget(self, text, secondary_text):
-        print(9)
         w = TwoLineListItem(text=text, secondary_text=secondary_text)
+        w.bind(on_release=lambda x: self.show_student_info(w.text[0]))
         self.ids.main_profiles.add_widget(w)
 
+    def show_student_info(self, index):
+        with open(f'data/profiles/{index}/prof.json', 'r', encoding='utf-8') as prof:
+            prof.seek(0)
+            data = json.load(prof)
+
+            self.ids.profile_photo.source = data['photo']
+
+            self.create_tab('Основаная информация', [data['name'], data['surname'], data['lastname']])
+            is_work = "работает" if data['is_working'] else "не работает"
+            self.create_tab('Иформация о работе', [str(data['age']), is_work, data['job_place'], data['position'], data['phone']])
+
+        self.current = 'profile'
+
+    def create_tab(self, title, texts):
+        tab = Tab(title=title)
+        for text in texts:
+            tab.add_label(text)
+        self.ids.profile_mdtabs.add_widget(tab)
+
+    # def clear_tabs(self):
+    #     self.ids.profile.remove_widget(self.ids.profile_mdtabs)
+
     def update_list(self):
-        for i in range(self.index):
+        for i in range(len(self.ids.main_profiles.children), self.index):
             with open(f'data/profiles/{i}/prof.json', 'r', encoding='utf-8') as prof:
                 prof.seek(0)
                 d = json.load(prof)
-                text = d['name'] + " " + d['surname'] + " " + d['lastname']
-                secondary_text = d['age'] + " " + "работает" if d['is_working'] else "не работает"
+                text = str(i) + " " + d['name'] + " " + d['surname']
+                secondary_text = d['age'] + " лет " + "работает" if d['is_working'] else "не работает"
                 self.create_list_widget(text, secondary_text)
 
 
@@ -105,7 +148,6 @@ class WindowManager(ScreenManager):
             json.dump(self.prof_info, prof)
         self.__set_index()
         self.index = self.__get_index()
-        self.update_list()
 
 
     def file_manager_open(self):
@@ -139,16 +181,15 @@ class WindowManager(ScreenManager):
                 self.file_manager.back()
         return True
 
+
 class MainApp(MDApp):
     def build(self):
-        # if platform == 'android':
-        #     from android.permissions import request_permissions, Permission
-        #     request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'DeepPurple'
-        # self.theme_cls.accent_palette = 'Red'
         return Builder.load_file("main.kv")
 
+    def on_start(self):
+        self.root.update_list()
 
 if __name__ == "__main__":
     MainApp().run()
